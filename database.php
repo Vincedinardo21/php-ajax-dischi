f52bfef2cd3a4fa46ac4c9e683d399cb9024a8b4
vince <vincedinardo21@gmail.com>
2022-06-29 15:35:38 +0200

    Database

---
+++ database.php
@@ -0,0 +1,64 @@
+<?php
+
+$dischi = [
+    [
+        "image" => "",
+        "album" => "New Jersey",
+        "author" => "Bon Jovi",
+        "year" => 1988,
+    ],
+    [
+        "image" => "",
+        "album" => "Live at Wembley 86",
+        "author" => "Queen",
+        "year" => 1992,
+    ],
+    [
+        "image" => "",
+        "album" => "Ten's Summoner's Tales",
+        "author" => "Sting",
+        "year" => 1993,
+    ],
+    [
+        "image" => "",
+        "album" => "Steve Gadd Band",
+        "author" => "Steve Gadd Band",
+        "year" => 2018,
+    ],
+    [
+        "image" => "",
+        "album" => "Brave New World",
+        "author" => "Iron Maiden",
+        "year" => 2000,
+    ],
+    [
+        "image" => "",
+        "album" => "One more car, one more rider",
+        "author" => "Eric Clapton",
+        "year" => 2002,
+    ],
+    [
+        "image" => "",
+        "album" => "Made in Japan",
+        "author" => "Deep Purple",
+        "year" => 1972,
+    ],
+    [
+        "image" => "",
+        "album" => "And justice for all",
+        "author" => "Metallica",
+        "year" => 1988,
+    ],
+    [
+        "image" => "",
+        "album" => "Hard wired",
+        "author" => "Dave Weckl",
+        "year" => 1994,
+    ],
+    [
+        "image" => "",
+        "album" => "Bad",
+        "author" => "Michael Jackson",
+        "year" => 1987,
+    ],
+];
